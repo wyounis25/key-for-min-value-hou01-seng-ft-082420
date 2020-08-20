ef259ef77a603b6931685ac2504bc0e9b4cc7fae
walid younis <walid.younis.90@gmail.com>
2020-08-20 03:43:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+lowest_value = 1000
+ lowest_key = nil
+ 
+ name_hash.each do |key,value|
+   if  value < lowest_value
+     lowest_value = value 
+   
+   end
+ end
+lowest_value
+end 
